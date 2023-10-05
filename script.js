5f513c2dd3620fbda4bfb6159bdd1fc5a9f3509a
Hussnain Raza Malhi <hussnainmalhi5@gmail.com>
2023-10-05 13:51:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+"strict mode"
+
+//Arrays
+
+//Task 1
+
+
+
+/*let arr = [];
+
+
+for (let i = 0; i < 5; i++) {
+
+    let numbers = Number(prompt("Enter five numbers"));
+
+    arr.push(numbers);
+
+}
+console.log(arr)
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+}
+
+
+console.log(sum); */
+
+
+
+//Task 2
+
+
+/*function findMax(arr) {
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    console.log("maximum number is " +
+        max)
+
+}
+let myArray = [1, 2, 3, 4]
+findMax(myArray)*/
+
+
+//Task 3
+
+
+
+/* let sentence = prompt("Write any sentence");
+let arr = [sentence];
+console.log(arr);
+for (let i = 0; i < arr.length; i++) {
+
+
+    console.log(arr[i].length);
+} */
+
+
+//Task 4
+
+/*function findAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        sum = sum + arr[i];
+    }
+    console.log("average of numbers is " + sum / arr.length)
+}
+let myArray = [1, 2, 3, 4];
+findAverage(myArray);*/
+
+
+//Task 5
+
+
+/*let result;
+
+function Multiple(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 3 == 0 || arr[i] % 5 == 0) {
+            result = console.log(arr[i]);
+        }
+    }
+}
+console.log(result);
+let input = [1, 2, 3, 5, 15]
+Multiple(input);*/
